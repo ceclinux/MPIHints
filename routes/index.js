7a10ad56413892af466c8c3a0653ac803cefefb5
ceclinux <src655@gmail.com>
2014-01-05 21:59:45 +0800

    登入和登出

--- routes/index.js
+++ routes/index.js
@@ -42,8 +42,28 @@ exports.doReg = function  (req,res) {
 }
 
 exports.login = function  (req,res) {
+    res.render('login',{title:'用户登入'});
 }
 exports.doLogin = function  (req,res) {
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(req.body.password).digest('base64');
+
+    User.get(req.body.username,function  (err,user) {
+
+        if (!user) {req.flash('error','用户不存在');return res.redirect('/login');
+        }
+        if (user.password != password) {
+            req.flash('error','用户口令错误');
+            return res.redirect('/login');
+        }
+        req.session.user = user;
+        req.flash('success','登陆成功');
+        res.redirect('/');
+
+    });
 }
 exports.logout = function  (req,res) {
+    req.session.user = null;
+    req.flash('success','登出成功');
+    res.redirect('/');
 }
