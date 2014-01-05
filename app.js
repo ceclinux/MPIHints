82e2dfc7c21790e4b0abd1edf0b6fd147b909346
ceclinux <src655@gmail.com>
2014-01-05 21:04:52 +0800

    可以注册啦

--- app.js
+++ app.js
@@ -12,24 +12,40 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var util = require('util');
+var flash = require('connect-flash');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(partials());
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.bodyParser());
-app.use(express.cookieParser());
-app.use(express.session({secret:settings.cookieSecret,store:new MongoStore({db:settings.db})}));
+app.configure(function  () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+    app.use(partials());
+    app.use(flash());
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({secret:settings.cookieSecret,store:new MongoStore({db:settings.db})}));
+    app.use(function  (req,res,next) {
+        res.locals.user = req.session.user;
+        var err = req.flash('error');
+        
+        res.locals.error = err.length ? err:null;
+        var succ = req.flash('success');
+        res.locals.success = succ.length ? succ:null;
+        next();
+    })
+
+
+    app.use(app.router);
+
+
+});
 
 // development only
 if ('development' == app.get('env')) {
@@ -46,3 +62,5 @@ app.post('/login',routes.doLogin);
 app.get('/logout',routes.logout);
 
 app.listen(3000);
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-
+var User = require('../models/user');
+var crypto = require('crypto');
 /*
  * GET home page.
  */
@@ -10,13 +11,36 @@ exports.index = function  (req,res) {
 
 exports.user = function  (req,res) {
 }
+
 exports.post = function  (req,res) {
 }
 exports.reg = function  (req,res) {
-    res.render('reg',{title:'rigister',"listitem":listitem});
-}
+    res.render('reg',{title:'Rigister',"listitem":listitem});
+};
+
 exports.doReg = function  (req,res) {
+    var md5 = crypto.createHash('md5');
+    var password = md5.update(req.body.password).digest('base64');
+    var newUser = new User({name: req.body.username,password:password});
+
+    User.get(newUser.name,function  (err,user) {
+        if (user) {err = 'Username already exists';}
+        if (err) {req.flash('error',err);
+            return res.redirect('/reg');
+        }
+
+        newUser.save(function  (err) {
+            if (err) {req.flash('error',err);
+                return res.redirect('/reg');
+            }
+            req.session.user = newUser;
+            req.flash('success','注册成功');
+            res.redirect('/');
+
+        });
+    });
 }
+
 exports.login = function  (req,res) {
 }
 exports.doLogin = function  (req,res) {
